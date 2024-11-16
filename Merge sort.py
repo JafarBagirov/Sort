0544d641e4604c30591d38fd325cb8e3c0133d22
Cefer Bagirov <143965076+JafarBagirov@users.noreply.github.com>
2024-11-16 02:41:26 -0800

    Update Merge sort.py

--- Merge sort.py
+++ Merge sort.py
@@ -1,15 +1,20 @@
 # merge sort
 # siyahını iki hissəyə ayıraraq hər hissəni ayrı sıralayır və birləşdirir 
 
-lst = [38, 27, 43, 3, 9, 82, 10]
-while len(lst) > 1:
-    mid = len(lst) // 2
-    left = lst[:mid]
-    right = lst[mid:]
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst  # Əsas hal: tək elementli siyahı artıq sıralıdır.
 
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])  # Sol hissəni rekursiv sırala.
+    right = merge_sort(lst[mid:])  # Sağ hissəni rekursiv sırala.
+
+    return merge(left, right)
+
+def merge(left, right):
     merged = []
     i = j = 0
-    
+
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
             merged.append(left[i])
@@ -18,8 +23,11 @@ while len(lst) > 1:
             merged.append(right[j])
             j += 1
 
+    # Qalan elementləri əlavə et
     merged += left[i:]
     merged += right[j:]
-    
-    lst = merged
-print(lst)
+    return merged
+
+lst = [38, 27, 43, 3, 9, 82, 10]
+sorted_lst = merge_sort(lst)
+print(sorted_lst)
