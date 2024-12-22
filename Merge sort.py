229de2183624a91b975d23cfea980090a6b290a3
Cefer Bagirov <143965076+JafarBagirov@users.noreply.github.com>
2024-12-22 22:52:40 +0400

    Update Merge sort.py

--- Merge sort.py
+++ Merge sort.py
@@ -29,3 +29,41 @@ def merge(left, right):
 lst = [38, 27, 43, 3, 9, 82, 10]
 sorted_lst = merge_sort(lst)
 print(sorted_lst)
+
+# II method non rekursiv _______________________________________
+arr = [38, 27, 43, 3, 9, 82, 10, -8, -89, -56, -75, 46, 443, 4, 3, 4, 3, 4, 95, 6, 11654, 612, 64, 6]
+
+n = len(arr)
+hisse = 1
+while hisse < n:
+    for left_sratr in range(0, n, hisse*2):
+        mid = min(left_sratr+hisse-1, n-1)
+        right_end = min(left_sratr + hisse*2-1, n-1)
+        n1 = mid-left_sratr+1
+        n2 = right_end - mid
+        L = arr[left_sratr:left_sratr+n1]
+        R = arr[mid+1:mid+1+n2]
+        i=j=0
+        k = left_sratr
+
+
+
+        while i < n1 and j < n2:
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < n1:
+            arr[k] = L[i] 
+            i += 1
+            k += 1
+        while j < n2:
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    hisse *= 2
+print(arr)
+
